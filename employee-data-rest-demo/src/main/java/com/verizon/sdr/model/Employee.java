6f28edbc2e10ebcd666d8148f62bf2f0cabb8428
LaboniDey <41467066+LaboniDey@users.noreply.github.com>
2018-10-22 16:42:36 +0530

    Add files via upload

---
+++ employee-data-rest-demo/src/main/java/com/verizon/sdr/model/Employee.java
@@ -0,0 +1,50 @@
+package com.verizon.sdr.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	private long employeeId;
+	private String empName;
+	private double basicSalary;
+	private double hra;
+	private String department;
+	
+	public long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getBasicSalary() {
+		return basicSalary;
+	}
+	public void setBasicSalary(double basicSalary) {
+		this.basicSalary = basicSalary;
+	}
+	public double getHra() {
+		return hra;
+	}
+	public void setHra(double hra) {
+		this.hra = hra;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	
+
+}
